7ef767d217d363379b4d72de401adf4d64611619
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-03-18 21:57:53 +0700

    add api

--- main.go
+++ main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 	chunk "redis-stream-demo/src/chunk"
 	"redis-stream-demo/src/config"
 	redisclient "redis-stream-demo/src/pkg/redis"
+	"redis-stream-demo/src/pkg/util"
 	"redis-stream-demo/src/stream"
 	"redis-stream-demo/src/zset"
+	"strconv"
+	"time"
+
+	"github.com/sony/sonyflake"
 )
+
+func generateHandler(w http.ResponseWriter, r *http.Request) {
+	countStr := r.URL.Query().Get("count")
+	count, err := strconv.Atoi(countStr)
+	if err != nil || count <= 0 {
+		http.Error(w, "Tham số count không hợp lệ", http.StatusBadRequest)
+		return
+	}
+
+	bucketStr := r.URL.Query().Get("bucket")
+	bucket, err := strconv.Atoi(bucketStr)
+	if err != nil || bucket <= 0 {
+		http.Error(w, "Tham số bucket không hợp lệ", http.StatusBadRequest)
+		return
+	}
+
+	var st sonyflake.Settings
+	// FIX CỨNG mốc thời gian để chống lỗi đồng hồ chạy lùi khi restart
+	st.StartTime = time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	sf := sonyflake.NewSonyflake(st)
+
+	logsEntry := util.GenerateData(sf, count)
+
+	type sidBucket struct {
+		Sid    uint64 `json:"sid"`
+		Bucket uint64 `json:"bucket"`
+	}
+
+	sidBuckets := []sidBucket{}
+	for _, logEntry := range logsEntry {
+		sidBuckets = append(sidBuckets, sidBucket{
+			Sid:    logEntry.SID,
+			Bucket: (logEntry.SID / uint64(bucket)) * uint64(bucket),
+		})
+	}
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"sids":      sidBuckets,
+		"createdAt": time.Now().Format("2006-01-02 15:04:05"),
+	})
+}
 
 func main() {
 	cfg, err := config.LoadConfig()
@@ -21,6 +69,8 @@ func main() {
 	}
 	log.Println("Running in mode: ", cfg.Server.Mode)
 
+	http.HandleFunc("/api/sids", generateHandler)
+
 	switch cfg.Server.Mode {
 	case "zset":
 		zset.Routes()
@@ -29,4 +79,5 @@ func main() {
 	default:
 		chunk.Routes()
 	}
+
 }
